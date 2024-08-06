71cddabf3b5d7ed1498700f8ea708d9c94e7de7f
Cyber Pirates <hpirates24@gmail.com>
2024-08-06 22:23:41 +0600

    Update ipt.py

--- ipt.py
+++ ipt.py
@@ -1,4 +1,9 @@
 import requests
+import os
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init(autoreset=True)
 
 # Function to get IP information
 def get_ip_info(ip_address):
@@ -14,19 +19,39 @@ def get_ip_info(ip_address):
 # Function to display IP information
 def display_ip_info(ip_info):
     if ip_info and ip_info['status'] == 'success':
-        print(f"IP Address: {ip_info.get('query')}")
-        print(f"City: {ip_info.get('city')}")
-        print(f"Region: {ip_info.get('regionName')}")
-        print(f"Country: {ip_info.get('country')}")
-        print(f"Location: {ip_info.get('lat')}, {ip_info.get('lon')}")
-        print(f"ISP: {ip_info.get('isp')}")
-        print(f"Organization: {ip_info.get('org')}")
-        print(f"Timezone: {ip_info.get('timezone')}")
+        print(f"{Fore.YELLOW}IP Address: {ip_info.get('query')}")
+        print(f"{Fore.YELLOW}City: {ip_info.get('city')}")
+        print(f"{Fore.YELLOW}Region: {ip_info.get('regionName')}")
+        print(f"{Fore.YELLOW}Country: {ip_info.get('country')}")
+        print(f"{Fore.YELLOW}Location: {ip_info.get('lat')}, {ip_info.get('lon')}")
+        print(f"{Fore.YELLOW}ISP: {ip_info.get('isp')}")
+        print(f"{Fore.YELLOW}Organization: {ip_info.get('org')}")
+        print(f"{Fore.YELLOW}Timezone: {ip_info.get('timezone')}")
     else:
-        print("Could not retrieve information for the given IP address.")
+        print(f"{Fore.RED}Could not retrieve information for the given IP address.")
 
 # Main function
 def main():
+    # Clear the terminal screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    # Display banner
+    banner = """
+    ██╗██████╗ ████████╗
+    ██║██╔══██╗╚══██╔══╝
+    ██║██████╔╝   ██║   
+    ██║██╔═══╝    ██║   
+    ██║██║        ██║   
+    ╚═╝╚═╝        ╚═╝   
+                        """
+    print(Fore.GREEN + banner)
+    
+    # Display additional information
+    print(f"{Fore.GREEN}Coded by: Ahraf Uzzaman")
+    print(f"{Fore.GREEN}Author: Cyber Pirates")
+    print(f"{Fore.GREEN}Github: https://github.com/CyberPirates24")
+    print("\n")
+
     ip_address = input("Enter the IP address: ")
     ip_info = get_ip_info(ip_address)
     display_ip_info(ip_info)
